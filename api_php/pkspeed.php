01a5706496f59f58c4859d789196290e1a6cd2fc
nodejshyperbk <115861934+nodejshyperbk@users.noreply.github.com>
2022-10-18 11:56:15 +0530

    pkspeed change

--- api_php/pkspeed.php
+++ api_php/pkspeed.php
@@ -1,9 +1,16 @@
 <?php
+if(isset($_GET['id'])){
+$id = $_GET['id'];
+
 $ua="Mozilla/5.0 (Windows NT 10.0; rv:86.0) Gecko/20100101 Firefox/86.0";
-$l="https://pkembed.com/yjld6q0vvaw9.html"; 
-$l="https://pkembed.com/embed-yjld6q0vvaw9.html";
+//$l="https://pkembed.com/2nbnfkg9s2fb.html";
+
+$l="https://pkembed.com/embed-". $id .".html";
+
 preg_match("/(\w+)\.html/",$l,$c);
+
 $l="https://pkembed.com/".$c[0];
+
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $l);
@@ -20,8 +27,23 @@ curl_close($ch);
 require_once("JavaScriptUnpacker.php");
 $jsu = new JavaScriptUnpacker();
 $out = $jsu->Unpack($h);
-
 preg_match("/src:\"([^\"]+)\"/",$out,$m);
-$link=$m[1];
-echo $link;
+$link= $m[1];
+
+if($m[1] != null){
+$data = [ 'code' => '1', 'url' => $m[1] ];
+}else{
+$data = [ 'code' => '0', 'url' => 'Not Found...' ];
+}
+
+header('Content-type: application/json');
+echo json_encode( $data );
+
+}else{
+    $data = [ 'code' => '0', 'url' => 'Not Found...' ];
+
+
+    header('Content-type: application/json');
+    echo json_encode( $data );
+}
 ?>
